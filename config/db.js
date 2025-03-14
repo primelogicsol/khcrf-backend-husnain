d4ed0ec7bdf32c45ad983caa4c20a8955355a5eb
Ali Haider <alihaidercheema08@gmail.com>
2025-03-14 22:41:22 +0500

    Changes

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "foundation12",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
+  process.env.DB_NAME || "hcrfdb",
+  process.env.DB_USER || "user@hcrf",
+  process.env.DB_PASSWORD || "1234@1234",
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "172.17.0.3",
     dialect: "mysql",  // ✅ Hardcoded for debugging
     port: process.env.DB_PORT || 3306,
     logging: false,
