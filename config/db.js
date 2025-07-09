1a7c386f13666f979fae1c8ce03ebadaa7da05d2
Ali_Cheema <119163298+alihaidercheemaa@users.noreply.github.com>
2025-07-10 03:40:50 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER || "user@hcrf",
   process.env.DB_PASSWORD || "1234@1234",
   {
-    host: process.env.DB_HOST || "172.17.0.1",
+    host: process.env.DB_HOST || "172.17.0.2",
     dialect: "mysql",  // ✅ Hardcoded for debugging
     port: process.env.DB_PORT || 3306,
     logging: false,
