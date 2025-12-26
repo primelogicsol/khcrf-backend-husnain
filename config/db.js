20c4c80c8419b29801b0e4c624b159e2a8caee3d
Ali_Cheema <119163298+alihaidercheemaa@users.noreply.github.com>
2025-12-26 06:16:10 +0500

    Update database configuration for Sequelize

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME || "hcrfdb",
-  process.env.DB_USER || "user@hcrf",
-  process.env.DB_PASSWORD || "1234@1234",
+  process.env.DB_USER || "hcrf_user",
+  process.env.DB_PASSWORD || "bold@321@321",
   {
-    host: process.env.DB_HOST || "172.17.0.2",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",  // ✅ Hardcoded for debugging
     port: process.env.DB_PORT || 3306,
     logging: false,
